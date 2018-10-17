47d2f15076c16063015f77fa3b1d3ee8b5cca1e3
Hedvika <hedvule@gmail.com>
2018-10-17 16:57:30 +0100

    Remember nose position

--- js/index.js
+++ js/index.js
@@ -47,6 +47,7 @@ function move_backward() {
 
 /************** Evaluate pose ****************/
 
+var prev_nose = null;
 // Evaluate nose to each ear distances to detect turning of face
 function evaluateFaceToEars(noseX, leftEarX, rightEarX){
     NoseToLeftEar = Math.abs(noseX - leftEarX);
@@ -63,6 +64,23 @@ function evaluateFaceToEars(noseX, leftEarX, rightEarX){
     }
 }
 
+function eval_nose (prev_pos, curr_pos){
+    if (Math.abs (curr_pos - prev_pos) < 10 | prev_pos === null) {
+        console.log("No move");
+        curr_state = "NO_MOVE";
+    }
+    else if (prev_pos < curr_pos) {
+        console.log("Right");
+        move_right();
+    }
+    else {
+        console.log("Left");
+        move_left();
+    }
+    prev_state = curr_state;
+
+}
+
 /************** Web browser webcam pose capture ****************/
 
 async function start() {
@@ -77,8 +95,9 @@ async function start() {
     setInterval(async() => {
         const pose = await model.estimateSinglePose(videoElement, imageScaleFactor, flipHorizontal, outputStride);
 
-        //console.log('nose', pose.keypoints[0].position)
-        // console.log('left eye', pose.keypoints[1].position)
+        // console.log('right wrist', pose.keypoints)
+        // console.log('right wrist', pose.keypoints[10].position)
+        // // console.log('left eye', pose.keypoints[1].position)
         // console.log('right eye', pose.keypoints[2].position)
         // console.log('left ear', pose.keypoints[3].position)
         // console.log('right ear', pose.keypoints[4].position)
@@ -87,11 +106,14 @@ async function start() {
         
         // Call function to evaluate snapshot for face turns
         // Turn if nose to left or right ear distances differ by more than 100 pixels
+        current_nose = pose.keypoints[0].position.x;
         noseX = pose.keypoints[0].position.x;
         leftEarX = pose.keypoints[3].position.x;
         rightEarX = pose.keypoints[4].position.x;
-        evaluateFaceToEars(noseX, leftEarX, rightEarX);
-    }, 1000);
+        // evaluateFaceToEars(noseX, leftEarX, rightEarX);
+        eval_nose(prev_nose, current_nose);
+        prev_nose = current_nose;
+    }, 10);
 }
 
 async function setupVideo() {
